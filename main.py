b36376fe812f35f11785ab4ccc588c7b0824e05e
Gaillard Florian <florian.gaillard@student.unamur.be>
2022-11-26 18:09:04 +0100

    fn dico modif fn frequency fini

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 
+global dyco_freq
 
-def temporary_dico(file):
+
+dyco_freq = {}
+def dico(file, dico):
     """
     Make temporary dictionary of word in a file
     parameter:
     ------------
     file: file we want to put his word in a dictionary (file)
+    dico: the dico we want to place word (dictionary)
     return:
     ------------
-    temp_dico : dictionary of word in a the file and number of time it appeared (dictionary)
+    dico : dictionary of word in the file and number of time it appeared (dictionary)
     """
     fh = open(file, 'r')
-    temp_dico = {}
     lines = fh.readlines()
     symbol = "()-:,;?!'^+-#=/*\"<>@.&[]{}%µ§_|~"
     for line in lines:
         for word in line.split():
-            word = ''.join(x for x in word if x not in symbol)
-            if word in temp_dico:
-                temp_dico[word] += 1
+            word = ''.join(x for x in word if x not in symbol or x != '')
+            if word in dico:
+                dico[word] += 1
             else:
-                temp_dico[word] = 1
+                dico[word] = 1
     fh.close()
-    #return temp_dico
+    print(dico)
+    return dico
 
-def annalyse():
+
+def analyse():
     ...
 
+
+def get_frequency():
+    themes = os.listdir('./archive_1/sorted')
+    for theme in themes:
+        for file in os.listdir(f'./archive_1/sorted/{theme}'):
+            dico(f'./archive_1/sorted/{theme}/{file}', dyco_freq)
+    themes = os.listdir('./archive_2/sorted')
+    for theme in themes:
+        for file in os.listdir(f'./archive_1/sorted/{theme}'):
+            dico(f'./archive_1/sorted/{theme}/{file}', dyco_freq)
+    themes = os.listdir('./archive_3/sorted')
+    for theme in themes:
+        for file in os.listdir(f'./archive_1/sorted/{theme}'):
+            dico(f'./archive_1/sorted/{theme}/{file}', dyco_freq)
+    themes = os.listdir('./archive_4/sorted')
+    for theme in themes:
+        for file in os.listdir(f'./archive_1/sorted/{theme}'):
+            dico(f'./archive_1/sorted/{theme}/{file}', dyco_freq)
+    return dyco_freq
